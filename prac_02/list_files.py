fd82b2a6cfc5765c8d3b8dd167d5e75ca82e95e0
Elliot <elliot.gram@learn.jcu.edu.au>
2024-06-02 20:46:25 +1000

    Change File location

--- prac_02/list_files.py
+++ prac_02/list_files.py
@@ -1,7 +1,7 @@
 import os
 
 print(f"The files and folders in {os.getcwd()} are:")
-items = os.listdir('.')
+items = os.listdir('../prac_01')
 for item in items:
     prefix = "(d) " if os.path.isdir(item) else "(f) "
     print(f"{prefix}\t{item}")
