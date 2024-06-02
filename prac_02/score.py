0b53e0af33d4cbfa13a09673562f0401e0a63a8a
Elliot <elliot.gram@learn.jcu.edu.au>
2024-06-02 20:56:20 +1000

    Add Functions

--- prac_02/score.py
+++ prac_02/score.py
@@ -4,16 +4,26 @@ Broken program to determine score status
 """
 
 
-score = float(input("Enter score: "))
-
-if 0 >= score or score > 100:
-    print("Invalid score")
+def main():
     score = float(input("Enter score: "))
+    check_score(score)
+    score_result(score)
 
-if score >= 90:
-    print("Excellent")
-elif score >= 50:
-    print("Passable")
-else:
-    print("Bad")
 
+def check_score(score):
+    if 0 >= score or score > 100:
+        print("Invalid score")
+        score = float(input("Enter score: "))
+
+
+def score_result(score):
+    if score >= 90:
+        print("Excellent")
+    elif score >= 50:
+        print("Passable")
+    else:
+        print("Bad")
+
+
+if __name__ == '__main__':
+    main()
