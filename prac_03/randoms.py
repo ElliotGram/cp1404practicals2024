a2b941c5d7990e7005853cc9cec3b697b60dd5ef
Elliot <elliot.gram@learn.jcu.edu.au>
2024-06-09 21:44:40 +1000

    Complete question 2

--- prac_03/randoms.py
+++ prac_03/randoms.py
@@ -4,3 +4,8 @@ import random
 print(random.randint(5, 20))  # line 1
 # Smallest: 5, Largest: 20
 
+# Printing a random number from the range 3 to 9 (inclusive), skipping by 2
+print(random.randrange(3, 10, 2))  # line 2
+# Smallest: 3, Largest: 9
+# No, line 2 could not produce a 4 since it skips by 2.
+
