fedc749548261e2d5641c8ba0d6a9697b2b92a76
Elliot <elliot.gram@learn.jcu.edu.au>
2024-06-09 22:08:33 +1000

    Add read code to read previous task

--- prac_03/files.py
+++ prac_03/files.py
@@ -2,3 +2,8 @@
 name = input("Enter your name: ")
 with open('name.txt', 'w') as file:
     file.write(name)
+
+# Read name from file and print greeting
+with open('name.txt', 'r') as file:
+    name = file.read()
+print(f"Hi {name}!")
