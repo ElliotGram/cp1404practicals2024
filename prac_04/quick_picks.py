f1f9789c8af1487f997722cb5d8eda20a7c0d0df
Elliot <elliot.gram@learn.jcu.edu.au>
2024-06-16 20:19:56 +1000

    Created main() function and createdthe while loop for generate quick picks function

--- prac_04/quick_picks.py
+++ prac_04/quick_picks.py
@@ -4,3 +4,17 @@ import random
 MIN_NUMBER = 1
 MAX_NUMBER = 45
 NUMBERS_PER_LINE = 6
+
+def main():
+    num_picks = int(input("How many quick picks? "))
+    generate_quick_picks(num_picks)
+
+
+def generate_quick_picks(num_picks):
+    for _ in range(num_picks):
+        # Generate a list of unique random numbers
+        numbers = []
+        while len(numbers) < NUMBERS_PER_LINE:
+            new_number = random.randint(MIN_NUMBER, MAX_NUMBER)
+            if new_number not in numbers:
+                numbers.append(new_number)
