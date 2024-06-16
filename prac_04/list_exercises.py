9fedc69bb88e201abd26b8b9e7d719d67a296974
Elliot <elliot.gram@learn.jcu.edu.au>
2024-06-16 20:13:34 +1000

    Added usernames constant

--- prac_04/list_exercises.py
+++ prac_04/list_exercises.py
@@ -1,15 +1,23 @@
-def main():
-    numbers = []
+# def main():
+#     numbers = []
+#
+#     for i in range(5):
+#         number = float(input("Number: "))
+#         numbers.append(number)
+#
+#     print(f"The first number is {numbers[0]}")
+#     print(f"The last number is {numbers[-1]}")
+#     print(f"The smallest number is {min(numbers)}")
+#     print(f"The largest number is {max(numbers)}")
+#     print(f"The average of the numbers is {sum(numbers) / len(numbers):.1f}")
+#
+#
+# main()
 
-    for i in range(5):
-        number = float(input("Number: "))
-        numbers.append(number)
 
-    print(f"The first number is {numbers[0]}")
-    print(f"The last number is {numbers[-1]}")
-    print(f"The smallest number is {min(numbers)}")
-    print(f"The largest number is {max(numbers)}")
-    print(f"The average of the numbers is {sum(numbers) / len(numbers):.1f}")
+# Define usernames as a constant list
+USERNAMES = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45',
+             'BaseInterpreterInterface', 'BaseStdIn', 'Command', 'ExecState',
+             'InteractiveConsole', 'InterpreterInterface', 'StartServer', 'bob']
 
 
-main()
