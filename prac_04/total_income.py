d6d020783420b653217a80638c7c216a165867f1
Elliot <elliot.gram@learn.jcu.edu.au>
2024-06-16 20:08:01 +1000

    Refactored months variable to num_months for clarity.

--- prac_04/total_income.py
+++ prac_04/total_income.py
@@ -9,16 +9,16 @@ def main():
     incomes = []
     months = int(input("How many months? "))
 
-    for month in range(1, months + 1):
-        income = float(input(f"Enter income for month {month}: "))
+    for num_month in range(1, months + 1):
+        income = float(input(f"Enter income for month {num_month}: "))
         incomes.append(income)
 
     print("\nIncome Report\n-------------")
     total = 0
-    for month in range(1, months + 1):
-        income = incomes[month - 1]
+    for num_month in range(1, months + 1):
+        income = incomes[num_month - 1]
         total += income
-        print("Month {:2} - Income: ${:10.2f} Total: ${:10.2f}".format(month, income, total))
+        print("Month {:2} - Income: ${:10.2f} Total: ${:10.2f}".format(num_month, income, total))
 
 
 main()
