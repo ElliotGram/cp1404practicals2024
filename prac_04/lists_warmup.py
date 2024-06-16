bd4221928dcf385a4ad8492bc784de2cd2ab6910
Elliot <elliot.gram@learn.jcu.edu.au>
2024-06-16 20:04:40 +1000

    Answered 4 questions

---
+++ prac_04/lists_warmup.py
@@ -0,0 +1,13 @@
+numbers = [3, 1, 4, 1, 5, 9, 2]
+
+# Q1
+numbers[0] = "ten"
+
+# Q2
+numbers[-1] = 1
+
+# Q3
+print(numbers[2:])  # Output: [4, 1, 5, 9, 2, 1]
+
+# Q4
+print(9 in numbers)  # Output: True
