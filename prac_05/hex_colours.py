f2230b901fad43e629998135e85417062bb834d3
Elliot <elliot.gram@learn.jcu.edu.au>
2024-06-23 14:19:52 +1000

    Added Starter File with 10 colours constants

---
+++ prac_05/hex_colours.py
@@ -0,0 +1,3 @@
+COLOURS = {"aliceblue": "#f0f8ff", "antiquewhite": "#faebd7", "aquamarine": "#7fffd4", "azure": "#f0ffff",
+           "beige": "#f5f5dc", "bisque": "#ffe4c4", "blanchedalmond": "#ffebcd", "blueviolet": "#8a2be2",
+           "burlywood": "#deb887", "cadetblue": "#5f9ea0"}
