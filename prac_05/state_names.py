a59c3831e354f6067d33b8d3137c0ed46917612a
Elliot <elliot.gram@learn.jcu.edu.au>
2024-06-23 14:21:04 +1000

    Changed Formatting to fit EAFP approach

--- prac_05/state_names.py
+++ prac_05/state_names.py
@@ -7,14 +7,15 @@ File needs reformatting
 
 CODE_TO_NAME = {"QLD": "Queensland", "NSW": "New South Wales", "NT": "Northern Territory", "WA": "Western Australia",
                 "ACT": "Australian Capital Territory", "VIC": "Victoria", "TAS": "Tasmania"}
-print(CODE_TO_NAME)
 
-state_code = input("Enter short state: ").upper()
-while state_code != "":
-    if state_code in CODE_TO_NAME:
-        print(state_code, "is", CODE_TO_NAME[state_code])
-    else:
+for code, name in CODE_TO_NAME.items():
+    print(f"{code} is {name}")
+
+while True:
+    state_code = input("Enter short state: ").upper()
+    if state_code == "":
+        break
+    try:
+        print(f"{state_code} is {CODE_TO_NAME[state_code]}")
+    except KeyError:
         print("Invalid short state")
-    for code, name in CODE_TO_NAME.items():
-        print(f"{code} is {name}")
-    state_code = input("Enter short state: ")
