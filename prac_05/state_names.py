8b64e7c80794cb20d583df7a27cc2eab946ca95b
Elliot <elliot.gram@learn.jcu.edu.au>
2024-06-23 14:22:34 +1000

    Corrected to not use a While True loop

--- prac_05/state_names.py
+++ prac_05/state_names.py
@@ -11,11 +11,12 @@ CODE_TO_NAME = {"QLD": "Queensland", "NSW": "New South Wales", "NT": "Northern T
 for code, name in CODE_TO_NAME.items():
     print(f"{code} is {name}")
 
-while True:
-    state_code = input("Enter short state: ").upper()
-    if state_code == "":
-        break
+state_code = input("Enter short state (or blank to quit): ").upper()
+
+while state_code != "":
     try:
         print(f"{state_code} is {CODE_TO_NAME[state_code]}")
     except KeyError:
         print("Invalid short state")
+
+    state_code = input("Enter short state (or blank to quit): ").upper()
