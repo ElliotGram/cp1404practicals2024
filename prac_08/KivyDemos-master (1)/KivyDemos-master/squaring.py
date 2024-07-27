23d86a0542a5c3110509fcdafedb9831f72b4280
Elliot <elliot.gram@learn.jcu.edu.au>
2024-07-27 14:25:33 +1000

    Changed display location of Squaring result

--- prac_08/KivyDemos-master (1)/KivyDemos-master/squaring.py
+++ prac_08/KivyDemos-master (1)/KivyDemos-master/squaring.py
@@ -16,6 +16,7 @@ class SquareNumberApp(App):
     """ SquareNumberApp is a Kivy App for squaring a number """
     def build(self):
         """ build the Kivy app from the kv file """
+        Window.size = (500, 400)
         self.title = "Square Number"
         self.root = Builder.load_file('squaring.kv')
         return self.root
