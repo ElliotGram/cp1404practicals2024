360157963a236fbb73eeeff8ad82924c8cf1ad82
Elliot <elliot.gram@learn.jcu.edu.au>
2024-07-27 22:26:26 +1000

    Gave the increase and decrease buttons functionality

--- prac_08/convert_miles_km.py
+++ prac_08/convert_miles_km.py
@@ -1,19 +1,20 @@
 """
 CP1404/CP5632 Practical
 Kivy GUI program to square a number
-Lindsay Ward, IT@JCU
-Started 13/10/2015
+Elliot Gram, IT@JCU
+Started 27/07/2024
 """
 
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.core.window import Window
 
-__author__ = 'Lindsay Ward'
+__author__ = 'Elliot Gram'
 
 
-class SquareNumberApp(App):
-    """ SquareNumberApp is a Kivy App for squaring a number """
+class MilesToKmConversion(App):
+    """ MilesToKmConversion is a Kivy App for converting Miles to Kilometers """
+
     def build(self):
         """ build the Kivy app from the kv file """
         Window.size = (500, 400)
@@ -29,5 +30,23 @@ class SquareNumberApp(App):
         except ValueError:
             pass
 
+    def increase_input(self, user_input):
+        """ Increase the value in the input field by 1 """
+        try:
+            user_input = float(user_input) + 1
+            self.root.ids.input_number.text = str(user_input)
+            self.handle_calculate(user_input)
+        except ValueError:
+            self.root.ids.output_label.text = "Invalid Input"
 
-SquareNumberApp().run()
+    def decrease_input(self, user_input):
+        """ Decrease the value in the input field by 1 """
+        try:
+            user_input = float(user_input) - 1
+            self.root.ids.input_number.text = str(user_input)
+            self.handle_calculate(user_input)
+        except ValueError:
+            self.root.ids.output_label.text = "Invalid Input"
+
+
+MilesToKmConversion().run()
