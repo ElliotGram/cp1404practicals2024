a8ce2bb529bd58fbf05526a1c8a833d38196d466
Elliot <elliot.gram@learn.jcu.edu.au>
2024-07-27 22:13:47 +1000

    Create starting code for the miles to km conversion Create visual layout for the app

---
+++ prac_08/convert_miles_km.py
@@ -0,0 +1,33 @@
+"""
+CP1404/CP5632 Practical
+Kivy GUI program to square a number
+Lindsay Ward, IT@JCU
+Started 13/10/2015
+"""
+
+from kivy.app import App
+from kivy.lang import Builder
+from kivy.core.window import Window
+
+__author__ = 'Lindsay Ward'
+
+
+class SquareNumberApp(App):
+    """ SquareNumberApp is a Kivy App for squaring a number """
+    def build(self):
+        """ build the Kivy app from the kv file """
+        Window.size = (500, 400)
+        self.title = "Miles to Kilometers"
+        self.root = Builder.load_file('convert_miles_km.kv')
+        return self.root
+
+    def handle_calculate(self, value):
+        """ handle calculation (could be button press or other call), output result to label widget """
+        try:
+            result = float(value) / 0.6214
+            self.root.ids.output_label.text = str(result)
+        except ValueError:
+            pass
+
+
+SquareNumberApp().run()
