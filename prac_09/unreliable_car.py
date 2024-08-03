1ce057e6074723e33e096e532ca32983833dacd6
Elliot <elliot.gram@learn.jcu.edu.au>
2024-08-03 18:35:27 +1000

    Add function to drive only if random number is less than reliability Added test code

--- prac_09/unreliable_car.py
+++ prac_09/unreliable_car.py
@@ -15,3 +15,11 @@ class UnreliableCar(Car):
         super().__init__(name, fuel)
         self.reliability = reliability
 
+    def drive(self, distance):
+        """Drive the car a given distance based on its reliability.
+
+        Only drive if a randomly generated number is less than the car's reliability.
+        """
+        if random.uniform(0, 100) < self.reliability:
+            return super().drive(distance)
+        return 0
