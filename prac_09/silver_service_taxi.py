d31669c3532b4a597578cbc333c9e2176ff164ea
Elliot <elliot.gram@learn.jcu.edu.au>
2024-08-03 18:48:55 +1000

    Add fare price and flagfall

--- prac_09/silver_service_taxi.py
+++ prac_09/silver_service_taxi.py
@@ -2,8 +2,14 @@ from prac_09.taxi import Taxi
 
 class SilverServiceTaxi(Taxi):
 
+    flagfall = 4.50
+
     def __init__(self, name, fuel, price_per_km, fanciness):
         """Initialise a SilverServiceTaxi instance."""
         super().__init__(name, fuel, price_per_km)
         self.fanciness = fanciness
         self.price_per_km *= fanciness
+
+    def get_fare(self):
+        """Return the price for the taxi trip including flagfall."""
+        return super().get_fare() + SilverServiceTaxi.flagfall
