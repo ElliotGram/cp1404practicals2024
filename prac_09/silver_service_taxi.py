667767f72fd222822e48587dbb08c6b4baddcf4b
Elliot <elliot.gram@learn.jcu.edu.au>
2024-08-03 18:49:33 +1000

    Add text to price silver service cost

--- prac_09/silver_service_taxi.py
+++ prac_09/silver_service_taxi.py
@@ -1,7 +1,7 @@
 from prac_09.taxi import Taxi
 
-class SilverServiceTaxi(Taxi):
 
+class SilverServiceTaxi(Taxi):
     flagfall = 4.50
 
     def __init__(self, name, fuel, price_per_km, fanciness):
@@ -13,3 +13,7 @@ class SilverServiceTaxi(Taxi):
     def get_fare(self):
         """Return the price for the taxi trip including flagfall."""
         return super().get_fare() + SilverServiceTaxi.flagfall
+
+    def __str__(self):
+        """Return a string like a Taxi but with the flagfall fee included."""
+        return (f"{super().__str__()} plus flagfall of ${SilverServiceTaxi.flagfall:.2f}")
