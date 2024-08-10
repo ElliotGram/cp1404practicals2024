31389096ecb1ab7a29bf6db1c1ca76b8c924c5b1
Elliot <elliot.gram@learn.jcu.edu.au>
2024-08-10 21:47:48 +1000

    Create wikipedia page retriever

--- prac_10/wiki.py
+++ prac_10/wiki.py
@@ -8,7 +8,21 @@ def main():
             get_wikipedia_page_details(title)
     print("Thank you.")
 
+
 def get_wikipedia_page_details(title):
+    try:
+        page = wikipedia.page(title, autosuggest=False)
+        print(f"{page.title}\n{page.summary}\n{page.url}")
+    except wikipedia.exceptions.DisambiguationError as e:
+        print("We need a more specific title. Try one of the following, or a new search:")
+        print(e.options)
+    except wikipedia.exceptions.PageError:
+        print(f'Page id "{title}" does not match any pages. Try another id!')
+    except wikipedia.exceptions.RedirectError:
+        print(f'The page "{title}" redirects to another page. Unable to retrieve information.')
+    except Exception as e:
+        print(f"An unexpected error occurred: {e}")
+
 
 if __name__ == "__main__":
     main()
